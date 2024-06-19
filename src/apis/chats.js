e142b339dde372ca39cc81ed95eb83409ed5267e
Seokjun Kang <kangsj0327@gmail.com>
2024-06-19 16:45:32 +0900

    feat: messages 저장, 불러오기 API 요청 함수 추가

--- src/apis/chats.js
+++ src/apis/chats.js
@@ -1,23 +1,6 @@
 import axios from "axios";
 import { Get, Post, Delete, Patch, Put } from ".";
 
-// export const getDistrictPosts = async (districtId: number) => {
-//     try {
-//       const res = await Get<CardData[]>(`/api/districts/${districtId}/posts`);
-//       if (res.status == 204) {
-//         return [];
-//       }
-//       return res.data.data;
-//     } catch (error) {
-//       if (axios.isAxiosError<CommonError>(error) && error.response) {
-//         const errorCode = error.response.data.errorCode;
-//         const message = error.response.data.message;
-//         console.log(`${errorCode}: ${message}`);
-//       }
-//     }
-//   };
-
-// getThreads 함수에서 Authorization 헤더 설정
 export const createThreads = async (reportYear, companyName, reportType) => {
   try {
     const token = process.env.REACT_APP_ACCESSTOKEN;
@@ -48,13 +31,58 @@ export const createThreads = async (reportYear, companyName, reportType) => {
 // getThreads 함수에서 Authorization 헤더 설정
 export const getThreads = async () => {
   try {
-    const token = process.env.REACT_APP_ACCESSTOKEN;
+    const token = process.env.REACT_APP_REFRESHTOKEN;
     const config = {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     };
     const res = await Get("/api/v1/chats", config);
+    return res.data.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const errorCode = error.response.data.errorCode;
+      const message = error.response.data.message;
+      console.log(`${errorCode}: ${message}`); // log
+    }
+  }
+};
+
+export const saveMessages = async (question, answer, chatRoomId) => {
+  try {
+    const token = process.env.REACT_APP_ACCESSTOKEN;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const res = await Post(
+      `/api/v1/chats/${chatRoomId}`,
+      {
+        question: question,
+        answer: answer,
+      },
+      config
+    );
+    return res.data.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const errorCode = error.response.data.errorCode;
+      const message = error.response.data.message;
+      console.log(`${errorCode}: ${message}`); // log
+    }
+  }
+};
+
+export const getMessages = async (chatRoomId) => {
+  try {
+    const token = process.env.REACT_APP_ACCESSTOKEN;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const res = await Get(`/api/v1/chats/${chatRoomId}`, config);
     return res.data.data;
   } catch (error) {
     if (axios.isAxiosError(error) && error.response) {
